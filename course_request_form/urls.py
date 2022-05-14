f3b868936af6fb3c5cb059b8868731aae75a63b0
Ben Rosen <benjamin.j.rosen@gmail.com>
2022-05-14 14:30:37 -0400

    init views and templates

--- course_request_form/urls.py
+++ course_request_form/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from form.views import SectionListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("sections/", SectionListView.as_view()),
 ]
--- form/views.py
+++ form/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Section
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the CRF index.")
+class SectionListView(ListView):
+    model = Section
