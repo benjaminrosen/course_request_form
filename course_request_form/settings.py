fd24e7695faf3877f46b9721df159a5c6cfb3224
Ben Rosen <benjamin.j.rosen@gmail.com>
2022-05-15 19:10:14 -0400

    set up for vanilla css

--- course_request_form/settings.py
+++ course_request_form/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = "course_request_form.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
