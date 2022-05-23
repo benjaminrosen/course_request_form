8c5ff6ec2fa18ec1c37de869bd4d1d96b5e87097
Ben Rosen <benjamin.j.rosen@gmail.com>
2022-05-23 13:51:18 -0400

    add canvas sites

--- form/views.py
+++ form/views.py
@@ -1,11 +1,24 @@
+from typing import cast
 from django.views.generic import DetailView, ListView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from config.config import PROD_URL
 
-from .models import Section
+from form.canvas import get_user_canvas_sites
+
+from .models import Section, User
 
 
 class HomePageView(LoginRequiredMixin, TemplateView):
     template_name = "form/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = cast(User, self.request.user)
+        context["email"] = user.email
+        context["courses"] = Section.objects.filter(instructors=user)
+        context["canvas_sites"] = get_user_canvas_sites(user.username)
+        context["canvas_url"] = f"{PROD_URL}/courses"
+        return context
 
 
 class SectionListView(ListView):
